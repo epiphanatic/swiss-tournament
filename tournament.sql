a77750a8a32ebce893ee48ffda98005cee80f46c
epiphanatic <simpson.jason.r@gmail.com>
2017-04-08 10:18:56 +1000

    Initial commit

---
+++ tournament.sql
@@ -0,0 +1,32 @@
+-- Table definitions for the tournament project.
+--
+
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament;
+CREATE TABLE players (
+  id SERIAL PRIMARY KEY,
+  name varchar(50) UNIQUE NOT NULL,
+  wins INTEGER DEFAULT 0,
+  matches INTEGER DEFAULT 0,
+  dateCreated TIMESTAMP DEFAULT current_timestamp
+);
+CREATE TABLE matches (
+  match_id SERIAL PRIMARY KEY,
+  winner INTEGER REFERENCES players(id),
+  loser INTEGER REFERENCES players(id)
+);
+CREATE VIEW standings AS
+SELECT players.id, players.name,
+  (SELECT COUNT(matches.winner)
+   FROM matches
+   WHERE players.id = matches.winner)
+   AS wins,
+
+  (SELECT COUNT(matches.match_id)
+   FROM matches
+   WHERE players.id = matches.winner
+   OR players.id = matches.loser)
+  AS matches
+FROM players
+ORDER BY wins, matches;
